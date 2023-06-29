8eebd40ea628b71cf03ee61af922b45567434240
gokhul-b <gokhulb2001@gmail.com>
2023-06-29 14:29:01 +0530

    routeerror1

--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AdminLogin = () => {
@@ -8,32 +8,26 @@ const AdminLogin = () => {
   const [isTokenFixed, setIsTokenFixed] = useState(false);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    if (isTokenFixed) {
-      navigate("/addflights");
-    }
-  }, [isTokenFixed, navigate]);
+  // useEffect(() => {
+  //   if (isTokenFixed) {
+  //     navigate("/addflights");
+  //   }
+  // }, [isTokenFixed, navigate]);
 
   const handleAdminLogin = (e) => {
     e.preventDefault();
     const query = { email: email, password: password };
     const url = "https://weak-pear-magpie.cyclic.app/adminlogin";
-    const doNavigate = () => {
-      setTimeout(() => {
-        navigate("/addflights");
-      }, 2000);
-      localStorage.setItem("token", true);
-    };
     const isAuthenticated = async () => {
       try {
         const response = await axios.get(url, { params: query });
         const bool = response.data;
         console.log(bool);
         if (bool) {
-          // localStorage.setItem("token", true);
+          localStorage.setItem("token", true);
           setIsTokenFixed(true);
-          // navigate("/addflights");
-          doNavigate();
+          navigate("/addflights");
+          // doNavigate();
         } else {
           setEmail("");
           setPassword("");
@@ -52,6 +46,9 @@ const AdminLogin = () => {
       }
     };
     isAuthenticated();
+    if (isTokenFixed) {
+      navigate("/addflights");
+    }
   };
   // const navi = async() => {
   //     localStorage.setItem('token',true)
@@ -59,10 +56,7 @@ const AdminLogin = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-200">
       <div className="max-w-md mx-auto sm:w-[400px]">
-        <form
-          onSubmit={handleAdminLogin}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col space-y-4"
-        >
+        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col space-y-4">
           <h2 className="text-center font-medium">Admin Login</h2>
           <input
             type="email"
@@ -80,6 +74,7 @@ const AdminLogin = () => {
           />
           <button
             type="submit"
+            onClick={handleAdminLogin}
             className="bg-blue-500 text-white font-semibold py-2 px-4 rounded"
           >
             Login
